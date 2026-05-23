721bb1e99d6fb56ca76e4393df122bfc5d6708ad
Eewin <eewin@example.com>
2026-05-23 10:03:12 +0800

    移除 Python 和 bat 脚本文件（不应在仓库中）

--- check_java.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set CLASSPATH=%~dp0gradle\wrapper\gradle-wrapper.jar
-"C:\Program Files\Java\jdk-22\bin\java.exe" -Xmx64m -Xms64m -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain properties -Dorg.gradle.java.home=C:/Program Files/Java/jdk-22 2>&1 | findstr /i "java.home jdk"
-echo.
-echo Actual JAVA_HOME env: %JAVA_HOME%
-"C:\Program Files\Java\jdk-22\bin\java.exe" -version
--- run_build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Java\jdk-17.0.4.1
-set PATH=%JAVA_HOME%\bin;%PATH%
-cd /d C:\Users\Administrator\WorkBuddy\20260501080626\bedrock_enhancement
-call gradlew.bat build 2>&1
